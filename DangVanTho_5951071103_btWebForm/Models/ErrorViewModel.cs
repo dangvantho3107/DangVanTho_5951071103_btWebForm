e65bae3eb535dac256c30b9b75fdea997ba46358
dangvantho3107 <dangvantho3107@gmail.com>
2021-03-22 23:13:55 +0700

    vantho

---
+++ DangVanTho_5951071103_btWebForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DangVanTho_5951071103_btWebForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
